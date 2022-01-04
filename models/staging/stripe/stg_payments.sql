c4b9ab94121b3c7d2af529ffa0782aa0d72c486c
Tom Murdoch <tom.murdoch@kinandcarta.com>
2022-01-04 20:10:38 +0000

    Sources

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -7,7 +7,7 @@ with payments as (
         amount / 100 as amount,
         created as created_at
 
-    from dbt-tutorial.stripe.payment
+    from {{ source('stripe', 'payment') }}
 )
 
 select * from payments
